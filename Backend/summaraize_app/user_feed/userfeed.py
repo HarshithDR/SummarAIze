039308ddb8460e5b07889d7fe1f59d2ef1c6d963
Sachin Shivanna <sachin.shivanna01@gmail.com>
2024-04-06 14:24:45 -0500

    commit9_Sachin

--- Backend/summaraize_app/user_feed/userfeed.py
+++ Backend/summaraize_app/user_feed/userfeed.py
@@ -4,11 +4,4 @@ import json
 def accessuserfeed(userId):
     return {'userId': userId, 'message': 'Im still under build'}
 
-def accessuserfeed(userId):
-    return 'Im still under build'
-
-import json
-
-def accessuserfeed(userId):
-    return {'userId': userId, 'message': 'Im still under build'}
 
