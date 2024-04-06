089c661f6a47b0ea59a1e80f8a79ed8181f431bd
Sachin Shivanna <sachin.shivanna01@gmail.com>
2024-04-06 04:14:35 -0500

    commit7_Sachin

--- Backend/summaraize_app/app.py
+++ Backend/summaraize_app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask,request
 from user_interest.userinterest import *
+from user_feed.userfeed import *
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
@@ -7,6 +8,12 @@ CORS(app)
 def userinterest():
     print(request.args)
     return set_user_interests(request.args.get('useremail'),request.args.get('user_interest'))
+
+
+@app.route('/newsfeed', methods=['GET'])
+def newsfeed():
+    print(request.args)
+    return accessuserfeed(request.args.get('user_id'))
 
 if __name__ == '__main__':
     # Run the app on http://localhost:5000/ by default
---
+++ Backend/summaraize_app/user_feed/userfeed.py
@@ -0,0 +1,2 @@
+def accessuserfeed(userId):
+    return 'Im still under build'
