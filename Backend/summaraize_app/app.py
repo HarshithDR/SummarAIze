16c1c899ae827d1c288c8dcaa26bf0733f3c4518
Suhas Palani <spalani3@hawk.iit.edu>
2024-04-06 14:16:40 -0500

    commit

--- Backend/summaraize_app/app.py
+++ Backend/summaraize_app/app.py
@@ -1,12 +1,21 @@
 from flask import Flask,request
 from user_interest.userinterest import *
+from user_feed.userfeed import *
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 @app.route('/interests', methods=['POST','GET'])
 def userinterest():
+    data = request.json
+    user_email = data.get('useremail')
+    user_interest = data.get('user_interest')
+    return set_user_interests(user_interest,user_email)
+
+
+@app.route('/newsfeed', methods=['GET'])
+def newsfeed():
     print(request.args)
-    return set_user_interests(request.args.get('useremail'),request.args.get('user_interest'))
+    return accessuserfeed(request.args.get('user_id'))
 
 if __name__ == '__main__':
     # Run the app on http://localhost:5000/ by default
