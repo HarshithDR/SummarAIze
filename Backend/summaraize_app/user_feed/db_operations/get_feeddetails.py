7d9ff5f4dabe343051fba387a902a4b3cd48363c
Sachin Shivanna <sachin.shivanna01@gmail.com>
2024-04-06 18:09:53 -0500

    fee_database_connect

--- Backend/summaraize_app/user_feed/db_operations/get_feeddetails.py
+++ Backend/summaraize_app/user_feed/db_operations/get_feeddetails.py
@@ -6,8 +6,8 @@ def get_feeddetails(interests,userId):
         cursor = db.cursor()
         # Constructing the placeholders for the IN clause
         placeholders = ', '.join(['%s' for _ in range(len(interests))])
-        sql = f"SELECT * FROM newsfeedinfo WHERE domain IN ({placeholders}) LIMIT 25;"
+        sql = f"SELECT * FROM summaraize.newsfeedinfo WHERE domain IN ({placeholders}) LIMIT 25;"
         cursor.execute(sql, (interests))
-        return {"user" : userId, "newsFeed" : cursor.fetchall()}
+        return {"success" : True, "user" : userId, "newsFeed" : cursor.fetchall()}
     except Exception as e:
         return {"success" : False, "error_message" : str(e)}
--- Backend/summaraize_app/user_feed/userfeed.py
+++ Backend/summaraize_app/user_feed/userfeed.py
@@ -1,7 +1,9 @@
-#from .
+from .db_operations.get_userinterests import *
+from .db_operations.get_feeddetails import *
 
 def accessuserfeed(userId):
-   # user_interests = get_userinterests(userId)
-    return {'userId': userId, 'message': 'Im still under build'}
+    user_interests = get_userinterests(userId)
+    lst_user_interests = user_interests.split(",")
+    return get_feeddetails(lst_user_interests,userId)
 
 
