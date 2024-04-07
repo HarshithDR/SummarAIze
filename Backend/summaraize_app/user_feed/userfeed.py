ffce883de684e0d05b868fe6d81429bf3fe82405
Sachin Shivanna <sachin.shivanna01@gmail.com>
2024-04-06 19:45:14 -0500

    commit_Sachin

--- Backend/summaraize_app/user_feed/userfeed.py
+++ Backend/summaraize_app/user_feed/userfeed.py
@@ -3,7 +3,9 @@ from .db_operations.get_feeddetails import *
 
 def accessuserfeed(userId):
     user_interests = get_userinterests(userId)
-    lst_user_interests = user_interests.split(",")
-    return get_feeddetails(lst_user_interests,userId)
+    print("here I am",user_interests)
+    #lst_user_interests = user_interests.split(",")
+    #get_feeddetails(lst_user_interests,userId)
+    return {'userId': userId, 'message': 'Im still under build'}
 
 
