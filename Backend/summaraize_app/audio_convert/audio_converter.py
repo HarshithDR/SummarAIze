2ac4927a2b7e493a1965a3da1a12708d4825ef88
Harshith <harshithdr10@gmail.com>
2024-04-06 22:00:09 -0500

    audio convertion complete

---
+++ Backend/summaraize_app/audio_convert/audio_converter.py
@@ -0,0 +1,11 @@
+from TTS.api import TTS
+
+class model_load:
+    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=True)
+    
+def converter(text):
+    model_load.tts.tts_to_file(text="Nischal says we will win and I believe it",
+                file_path="output.wav",
+                speaker_wav="reference_voice.wav",
+                language="en")
+    return "reference_voice.wav"
--- Backend/summaraize_app/flow.py
+++ Backend/summaraize_app/flow.py
@@ -1,8 +1,14 @@
 from text_collection_and_summarizer import newsapi
 from summarize import summarizer
+from audio_convert import audio_converter
 
-newsapi.newsapi_fun("bitcoin")
 
-summarizer.load_model
+# newsapi.newsapi_fun("bitcoin")
 
-summarizer.file_load()
+# summarizer.load_model
+
+# summarizer.file_load()
+
+audio_converter.model_load
+audio_converter.converter('Hi this is sample voice, i am working on integrating every component built by everyone.')
+
