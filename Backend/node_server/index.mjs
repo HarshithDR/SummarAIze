6f4af639494685ebe387b78da9e5087bae5fc229
Sachin Shivanna <sachin.shivanna01@gmail.com>
2024-04-06 22:01:05 -0500

    jsserver : Sachin

--- Backend/node_server/index.mjs
+++ Backend/node_server/index.mjs
@@ -1,18 +1,27 @@
 
 import BRX, { BRK } from 'brx-node';
 import { v_chat_ittr1 } from './schemas/brx_schema.mjs';
+export async function brx_image_generator (userprompt) {
+    const brx = new BRX('brx15ebf1c0ec82781eb4569a4a2b638706004bf94615ff5b4262726e7df737e5ac', { verbose: false });
 
-const brx = new BRX('brx15ebf1c0ec82781eb4569a4a2b638706004bf94615ff5b4262726e7df737e5ac', { verbose: false });
+    let brxObject = new BRK(v_chat_ittr1);
 
-let brxObject = new BRK(v_chat_ittr1);
-
-brxObject.input['input'].value = "ball";
-brxObject.input['image_var'].value = "bitcoin"; //change only this parameter
+    brxObject.input['input'].value = "ball";
+    brxObject.input['image_var'].value = "bitcoin"; //change only this parameter
 
 
-const result = await brx.execute(brxObject, (event) => {
-    console.log("Inline Event...")
-    console.log(event)
-});
-console.log("Final Result...")
-console.log(result)
+    await brx.execute(brxObject, (event) => {
+        console.log("Inline Event...")
+        console.log(event)
+    })
+    .then((result) => {
+        console.log("Result...")
+        console.log(result)
+        return {"success":true,"result":result}
+    }).catch((error) => {
+        console.log("Error...")
+        console.log(error)
+        return {"success":false,"result":result, "error":error}
+    });
+
+}
--- Backend/node_server/server.js
+++ Backend/node_server/server.js
@@ -1,12 +1,24 @@
 import express from 'express';
-//import {brxObject} from './index.mjs'
+import { parse } from 'url';
+import { parse as parseQuery } from 'querystring';
+import {brx_image_generator} from './index.mjs'
 const app = express();
 
 const PORT = 3000;
 
 app.get('/user_prompt', (req, res) => {
+    const parsedUrl = parse(req.url);
 
-    res.send('Hello, World!');
+    // Get the query string
+    const queryString = parsedUrl.query;
+
+    // Parse the query string to get the parameters
+    const queryParams = parseQuery(queryString);
+
+    // Get the value of the 'user_prompt' parameter
+    const userPromptValue = queryParams.user_prompt;
+    console.log(userPromptValue)
+    res.send(brx_image_generator(userPromptValue));
 });
 
 app.listen(PORT, () => {
