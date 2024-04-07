4ce83c73aa0a54ccb41f7c83a588fe288bbadbd8
Harshith <harshithdr10@gmail.com>
2024-04-06 20:02:37 -0500

    node update

---
+++ Backend/node_server/schemas/brx_schema.mjs
@@ -0,0 +1,41 @@
+export let v_chat_ittr1 = {
+  "description": "This is a simple color adder as an example",
+  "brxName": "Summaraize",
+	"brxId": "c87d8422-6554-4ffb-ba1e-50146a67c53a",
+  "dependantBrxIds": {},
+  "processType": 16,
+  "schemas": {
+    "mainBrxId": "2f2f590e-4c12-4d11-9c4e-fc30a033ffbf",
+    "schemas": {
+      "_isMap": true,
+      "data": [
+        [
+          "main_brx_entry_schema",
+          {
+            "schemaFields": {
+              "_isMap": true,
+              "data": [
+                [
+                  "input",
+                  {
+                    "fieldValueDataType": "string",
+                    "fieldValue": "testval"
+                  }
+                ],
+                [
+                  "image_var",
+                  {
+                    "fieldValueDataType": "string",
+                    "fieldValue": "testval"
+                  }
+                ]
+              ]
+            },
+            "brxName": "Summaraize",
+		        "brxId": "c87d8422-6554-4ffb-ba1e-50146a67c53a",
+          }
+        ]
+      ]
+    }
+  }
+}
